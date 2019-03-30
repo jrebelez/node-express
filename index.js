b6997d1bd4f8d64785d0a7999d0187fa9c1ef17d
Josh Rebelez <59lpjrebelez@gmail.com>
2019-03-30 10:50:23 -0700

    workshop 1

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const promoRouter = require('./routes/promoRouter');
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,7 +12,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 app.use('/dishes', dishRouter);
 app.use(express.static(__dirname+ '/public'))
 
